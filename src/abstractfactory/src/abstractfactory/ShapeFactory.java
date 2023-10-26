d32e98c5730e40fe4285bbfb31eb77e5fff3d52e
QAZI SAMI <qazi.sami.2021@gmail.com>
2023-10-26 09:00:46 +0500

    abstract factory
    implemented it and updated it

---
+++ src/abstractfactory/src/abstractfactory/ShapeFactory.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package abstractfactory;
+
+/**
+ *
+ * @author fa20-bse-024
+ */
+public class ShapeFactory extends AbstractFactory {
+   public Shape getShape(String shapeType){    
+      if(shapeType.equalsIgnoreCase("RECTANGLE")){
+         return new Rectangle();         
+      }else if(shapeType.equalsIgnoreCase("SQUARE")){
+         return new Square();
+      }	 
+      return null;
+   }
+
+   
+}
